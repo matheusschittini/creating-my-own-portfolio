f654c4939b82e1c996d4213d91121ab217e0b68b
Matheus Schittini <matheusschittinimc@gmail.com>
2023-10-02 21:45:19 -0300

    Changed component Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,8 +8,8 @@ const Card = ({ title, description, imageSrc }) => {
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-    <VStack>
-      <Image src={imageSrc}></Image>
+    <VStack backgroundColor="white" borderRadius="xl">
+      <Image src={imageSrc} borderRadius="xl"></Image>
       <VStack>
         <HStack>
           <Heading>{title}</Heading>
